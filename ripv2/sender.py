14c3d30a6e08ce2570ef7a2676ef61488400dc45
Alina <72575682+AlinaNistor@users.noreply.github.com>
2020-12-01 20:50:38 +0200

    update sender

--- ripv2/sender.py
+++ ripv2/sender.py
@@ -1,11 +1,12 @@
 import socket
 
+
 IP = "127.0.0.1"
-PORT = 520
+PORT = 2206
 MESSAGE = b"test message"
 
 
-class Sender():
+class Sender:
 
     def __init__(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
