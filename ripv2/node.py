147adc67df99c913f738c6a7a7d3e05838f3884c
Alina <72575682+AlinaNistor@users.noreply.github.com>
2020-12-02 23:03:43 +0200

    update node.py

--- ripv2/node.py
+++ ripv2/node.py
@@ -1,6 +1,24 @@
-import socket 
+import socket
+from pack import RipPack
+from route_entry import RouteEntry
+from header import Header
+
 
 class Node:
 
+    IP = socket.gethostbyname('0.0.0.0')
+    PORT = 520
+    BASE_TIMER = 5
+    MAX_METRIC = 16
+    ROUTE_TIMEOUT = BASE_TIMER * 6
+    DELETE_TIMEOUT = BASE_TIMER * 4
+
     def __init__(self):
-        self.routing_table = []
+        self.neighbours = []
+        self.routingTable = {}  # ipAddr : routeEntry
+
+    def init_socket(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    def update_routing_table(self):
+        pass
