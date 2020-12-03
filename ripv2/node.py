2bcec19759e726ad00222a4f21f6761b9e594adb
Simona Sirghi <maria-simona.sirghi@student.tuiasi>
2020-12-03 10:30:14 +0200

    modified node

--- ripv2/node.py
+++ ripv2/node.py
@@ -13,12 +13,29 @@ class Node:
     ROUTE_TIMEOUT = BASE_TIMER * 6
     DELETE_TIMEOUT = BASE_TIMER * 4
 
-    def __init__(self):
+    def __init__(self, ip: RouteEntry):
         self.neighbors = []
         self.routingTable = {}  # ipAddr : routeEntry
+        self.id = ip.ipAddress
 
     def init_socket(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind((self.IP, self.PORT))
 
-    def update_routing_table(self):
-        pass
+    def update_routing_table(self, packet: RipPack):
+        for route in packet.route_entries:
+            print(route)
+
+    def print_routing_table(self):
+        self.routingTable[self.id] = RouteEntry()
+        line = "-----------------------------------------------------------------------------"
+        print(line)
+        print("|      Routing Table   (Router " + str(socket.gethostbyname(socket.gethostname())) + ")      |")
+        print(line)
+        print("|Router IP  |  Metric  |  NextHop  |  Flag  |  Garbage |  Timeout |")
+        print(line)
+        for entry in self.routingTable:
+            print(self.routingTable[entry])
+            print(line)
+        print("--------------------------------------------------------------------------------")
+        print('\n')
